0ee654d59d58f0ca0b8ce2b4d32655343f2adc44
samasaleh5 <ss2104@fayoum.edu.eg>
2025-02-12 18:34:02 +0200

    Q3 solved

--- Program.cs
+++ Program.cs
@@ -10,7 +10,11 @@ namespace Assignment_LINQ
             // var result = ProductList.Where(P => P.UnitsInStock == 0);
             #endregion
             #region Q2
-            var result = ProductList.Where(P => P.UnitsInStock == 0 && P.UnitPrice>3.00m);
+            // var result = ProductList.Where(P => P.UnitsInStock == 0 && P.UnitPrice>3.00m);
+            #endregion
+            #region Q3
+            String[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            var result = Arr.Select((name, index) => new { Name = name, Index = index }).Where(item => item.Name.Length < item.Index);
             #endregion
 
             foreach (var item in result)
