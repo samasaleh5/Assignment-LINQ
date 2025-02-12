0107c65e585f85fe97643cea601b20e8b413187a
samasaleh5 <ss2104@fayoum.edu.eg>
2025-02-12 18:13:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using static Assignment_LINQ.ListGenerators;
+using System.Linq;
+namespace Assignment_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            var result = ProductList.Where(P => P.UnitsInStock == 0);
+            #endregion
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item}");
+
+            }
+
+        }
+    }
+}
