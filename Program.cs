28cf6a74726aa6482026d1bcf797dbf41e6c1e6d
samasaleh5 <ss2104@fayoum.edu.eg>
2025-02-12 19:01:36 +0200

    part2 Q3 solved

--- Program.cs
+++ Program.cs
@@ -27,11 +27,16 @@ namespace Assignment_LINQ
             //    Console.WriteLine($"First OutOfStockProduct is {result.ProductName}");
             #endregion
             #region Q2
-            var result = ProductList.FirstOrDefault(p => p.UnitPrice > 1000);
-            if (result !!= null)
-                Console.WriteLine($"First product over 1000: {result.ProductName} - ${result.UnitPrice}");
-            else
-                Console.WriteLine("NO Product Found with Price Over 1000");
+            //var result = ProductList.FirstOrDefault(p => p.UnitPrice > 1000);
+            //if (result !!= null)
+            //    Console.WriteLine($"First product over 1000: {result.ProductName} - ${result.UnitPrice}");
+            //else
+            //    Console.WriteLine("NO Product Found with Price Over 1000");
+            #endregion
+            #region Q3
+            int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var result=Arr.Where(n=>n>5).Skip(1).FirstOrDefault();
+            Console.WriteLine(result);
             #endregion
 
             //foreach (var item in result)
